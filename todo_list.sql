f96e91a24f2b1783e14be0dd170a630519d90a9b
Raimi_Yuuto <playzyehuda@gmail.com>
2024-10-25 14:42:51 +0700

    sql update

--- todo_list.sql
+++ todo_list.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 24, 2024 at 08:59 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Waktu pembuatan: 25 Okt 2024 pada 09.41
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login_user`
+-- Struktur dari tabel `login_user`
 --
 
 CREATE TABLE `login_user` (
@@ -36,17 +36,17 @@ CREATE TABLE `login_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `login_user`
+-- Dumping data untuk tabel `login_user`
 --
 
 INSERT INTO `login_user` (`id`, `username`, `password`, `email`, `birth_date`) VALUES
-(6, 'rifqi', '$2y$10$0U93KpjDyJ1QHfF2bvwJROSHp/hAYi030qrNsh6K2e.mZW.jZ36li', 'rifqi@gmail.com', '2024-10-10'),
-(7, 'depp', '$2y$10$qByl1WdXqFB.G8jVegpei.csSTDUe5ynhbgnkt5J0ytgikkl3VACO', 'davew1726@gmail.com', '2024-10-17');
+(8, 'User1', '$2y$10$20rtLHLRchvPl7vrefazqeqrMIQHydLQ1c1npusMonNeD2eeBXt1i', 'user1@gmail.com', '2024-10-10'),
+(9, 'User2', '$2y$10$zb9PtSwB32gBBEQ6915kz.uH6XFSIuCOpwoMGmqp5LONZh0.oQntm', 'user2@gmail.com', '2024-10-10');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tasks`
+-- Struktur dari tabel `tasks`
 --
 
 CREATE TABLE `tasks` (
@@ -62,10 +62,21 @@ CREATE TABLE `tasks` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data untuk tabel `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `description`, `status`, `due_date`, `participants`, `image_path`, `created_at`, `updated_at`) VALUES
+(27, 8, 'Makan', 'Nasi Goreng', 'To do', '2024-10-25', 'user 1', 'uploads/1729841816_Classroom, Arseniy Chebynkin.jfif', '2024-10-25 07:36:56', '2024-10-25 07:36:56'),
+(28, 8, 'Mandi', 'Mandi Pagi', 'To do', '2024-10-25', 'user 2', 'uploads/1729841869_66f38398faba38139e3b4c541f05c706.jpg', '2024-10-25 07:37:49', '2024-10-25 07:39:03'),
+(29, 8, 'Tidur', 'Tidur Malam', 'To do', '2024-10-25', 'user 1 dan user 2', 'uploads/1729841929_5e4e38699227551404f43f15f31e1e3f.jpg', '2024-10-25 07:38:49', '2024-10-25 07:38:49'),
+(30, 9, 'Main', 'Valorant', 'To do', '2024-10-25', 'user 1', 'uploads/1729842052_cbcb7204a0e2132d7744fcd8419d0989.jpg', '2024-10-25 07:40:52', '2024-10-25 07:40:52'),
+(31, 9, 'Belajar', 'Web Programming', 'To do', '2024-10-25', 'user 2', 'uploads/1729842096_6fef69e82ca2b101268ecf736d228f69.jpg', '2024-10-25 07:41:36', '2024-10-25 07:41:36');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `todos`
+-- Struktur dari tabel `todos`
 --
 
 CREATE TABLE `todos` (
@@ -81,53 +92,53 @@ CREATE TABLE `todos` (
 --
 
 --
--- Indexes for table `login_user`
+-- Indeks untuk tabel `login_user`
 --
 ALTER TABLE `login_user`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- Indexes for table `tasks`
+-- Indeks untuk tabel `tasks`
 --
 ALTER TABLE `tasks`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 
 --
--- Indexes for table `todos`
+-- Indeks untuk tabel `todos`
 --
 ALTER TABLE `todos`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `login_user`
+-- AUTO_INCREMENT untuk tabel `login_user`
 --
 ALTER TABLE `login_user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `tasks`
+-- AUTO_INCREMENT untuk tabel `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
--- AUTO_INCREMENT for table `todos`
+-- AUTO_INCREMENT untuk tabel `todos`
 --
 ALTER TABLE `todos`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tasks`
+-- Ketidakleluasaan untuk tabel `tasks`
 --
 ALTER TABLE `tasks`
   ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `login_user` (`id`) ON DELETE CASCADE;
